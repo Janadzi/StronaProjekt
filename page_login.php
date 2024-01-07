72d617a8a0ea5fc74e0a7f65b1829d863943dc14
KacperDawid <admaael@outlook.com>
2024-01-07 15:39:24 +0100

    Zmieniłem na swój serwer lokalny

--- page_login.php
+++ page_login.php
@@ -32,10 +32,15 @@
     if(isset($_POST['login']) && isset($_POST['password'])){    
         session_start();
 
-        $servername = "sql104.byethost11.com";
+        $servername = "localhost";
+        $username = "root";
+        $password = 'janadzi2023<<';
+        $dbname = 'projekt';
+
+        /*$servername = "sql104.byethost11.com";
         $username = "b11_35479412";
         $password = 'Llellb565634$gh';
-        $dbname = 'b11_35479412_eee';
+        $dbname = 'b11_35479412_eee';*/
 
         /*$servername = "127.0.0.1:3306";
         $username = "root";
--- page_update_password.php
+++ page_update_password.php
@@ -37,11 +37,15 @@
             if( $_POST['password'] == $_POST['password_repeat']){
             session_start();
             
+            $servername = "localhost";
+            $username = "root";
+            $password = 'janadzi2023<<';
+            $dbname = 'projekt';
 
-            $servername = "sql104.byethost11.com";
+            /*$servername = "sql104.byethost11.com";
             $username = "b11_35479412";
             $password = 'Llellb565634$gh';
-            $dbname = 'b11_35479412_eee';
+            $dbname = 'b11_35479412_eee';*/
 
             /*$servername = "127.0.0.1:3306";
             $username = "root";
